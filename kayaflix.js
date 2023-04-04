78c24413fee41f6ed9f6d7362116c92b013d2c41
hachikox <lahealataq@gmail.com>
2023-04-04 11:49:02 +0300

    Add files via upload

--- kayaflix.js
+++ kayaflix.js
@@ -1,4 +1,4 @@
-console.log("kayaflix-6")
+console.log("kayaflix-7")
 R7xOY[623438] = (function ()
 {
 	var b67 = 2;
@@ -77121,7 +77121,7 @@ R7xOY[949] = (function (b7W)
 									Tb: m().pf(N)
 								});
 							}
-							else(L.mediaKeys = new mb.WebKitMediaKeys(O), L.context.ea ? (L.log.trace("LICENSE: calling setmediakeys on video element"), L.context.ea.webkitSetMediaKeys(L.mediaKeys)) : (I(), L.Pq = new D.FD(), L.Pq.addEventListener("sourceopen", J), L.url = URL.createObjectURL(L.Pq), console.log(L.url, L.Pq), L.context.ea.src = L.url), M(
+							else(L.mediaKeys = new mb.WebKitMediaKeys(O), L.context.ea ? (L.log.trace("LICENSE: calling setmediakeys on video element"), L.context.ea.webkitSetMediaKeys(L.mediaKeys)) : (I(), L.Pq = new D.FD(), L.Pq.addEventListener("sourceopen", J), L.url = URL.createObjectURL(L.Pq), null, L.context.ea.src = L.url), M(
 							{
 								ja: !0,
 								mediaKeys: L.mediaKeys
@@ -158782,6 +158782,7 @@ R7xOY[949] = (function (b7W)
 						}
 						try
 						{
+							console.log(this.Jg, URL.createObjectURL(this.Jg));
 							this.Fm = URL.createObjectURL(this.Jg);
 						}
 						catch (J)
